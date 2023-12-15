432e6332ddcd053be4fa5f920480887ddc11cd52
c03452 <c0345@oic.jp>
2023-12-15 15:49:36 +0900

    まだできない

--- src/main/java/com/example/dto/UserDto.java
+++ src/main/java/com/example/dto/UserDto.java
@@ -1,39 +1,34 @@
 package com.example.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Table(name = "user")
 public class UserDto {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column
-	private int id;
+	//	@Id
+	//	@GeneratedValue(strategy = GenerationType.AUTO)
+	//	@Column
+	//	private int id;
 	private String email;
 	private String password;
 	private String role;
 	private String fullname;
 
-	public UserDto(int id, String email, String password, String role, String fullname) {
+	public UserDto(String email, String password, String role, String fullname) {
 		super();
-		this.id = id;
 		this.email = email;
 		this.password = password;
 		this.role = role;
 		this.fullname = fullname;
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
+	//	public int getId() {
+	//		return id;
+	//	}
+	//
+	//	public void setId(int id) {
+	//		this.id = id;
+	//	}
 
 	public String getEmail() {
 		return email;
